ec6232c4841a94caff98f6f6de59f9cfbb7f91f0
randomnovice7331 <educationalPersonal@proton.me>
2025-06-11 10:56:47 +0200

    Update app.py
    1. Pokušaj centriranja
    
    2. Širina slika je kao u originalu

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ import streamlit as st
 from PIL import Image
 
 
+st.markdown(
+    """
+    <style>
+      /* Uzmi glavni container i centriraj sve elemente */
+      .css-18e3th9 {            /* klasa se može mijenjati s promjenama Streamlit-a */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
+
 # 1) Širi layout ako već nisi
 st.set_page_config(layout="wide")
 st.title("Modeling Creatinine in Chronic kidney disease (SDE project)")
@@ -16,7 +32,7 @@ img4 = Image.open("Fig4_1800.webp")
 
 # 3) Prva slika manja
 st.subheader("Creatinin: raw data")
-st.image(img1, caption="", width=400)
+st.image(img1, caption="", use_column_width=True)
 
 # razmak
 st.markdown("<br>", unsafe_allow_html=True)
@@ -26,7 +42,7 @@ st.markdown("<br>", unsafe_allow_html=True)
 
 # 4) Druga slika veća
 st.subheader("Plot of data and simulation")
-st.image(img2, caption="", width=1000)
+st.image(img2, caption="", use_column_width=True)
 
 
 # razmak
@@ -38,7 +54,7 @@ st.markdown("<br><br>", unsafe_allow_html=True)
 
 # 5) Treća slika veća
 st.subheader("Form of the model and meaning of symbols")
-st.image(img3, caption="", width=1000)
+st.image(img3, caption="", use_column_width=True)
 
 
 # razmak
@@ -47,7 +63,7 @@ st.markdown("<br><br>", unsafe_allow_html=True)
 
 # 6) Četvrta slika veća
 st.subheader("Statistical test for residuals and estimation of error")
-st.image(img4, caption="", width=1000)
+st.image(img4, caption="", use_column_width=True)
 
 
 # razmak
