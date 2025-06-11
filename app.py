e1e459df20ddab8419997d94b80da1d5a7f535a5
randomnovice7331 <educationalPersonal@proton.me>
2025-06-11 10:29:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,59 @@
 import streamlit as st
 from PIL import Image
 
-# Lista točnih naziva tvojih slika
-images = [
-    "Fig1_1200.webp",
-    "Fig2_2000.webp",
-    "Fig3_1800.webp",
-    "Fig4_1800.webp"
-]
-captions = [
-    "Creatinin: raw data",
-    "Plot of data and simulation",
-    "Form of the model and meaning of symbols",
-    "Statistical test for residuals and estimation of error"
-]
 
-st.title("Modeling Creatinine concentration in the blood for a chronic kidney disease patient (SDE project)")
-for path, cap in zip(images, captions):
-    img = Image.open(path)
-    st.image(img, caption=cap, use_container_width=True)
+# 1) Širi layout ako već nisi
+st.set_page_config(layout="wide")
+st.title("Modeling Creatinine in Chronic kidney disease (SDE project)")
+
+
+# Lista točnih naziva tvojih slika
+# 2) Učitaj slike
+img1 = Image.open("Fig1_1200.webp")
+img2 = Image.open("Fig2_2000.webp")
+img3 = Image.open("Fig3_1800.webp")
+img4 = Image.open("Fig4_1800.webp")
+
+# 3) Prva slika manja
+st.subheader("Creatinin: raw data")
+st.image(img1, caption="", width=400)
+
+# razmak
+st.markdown("<br>", unsafe_allow_html=True)
+
+
+
+
+# 4) Druga slika veća
+st.subheader("Plot of data and simulation")
+st.image(img2, caption="", width=1000)
+
+
+# razmak
+st.markdown("<br><br>", unsafe_allow_html=True)
+
+
+
+
+
+# 5) Treća slika veća
+st.subheader("Form of the model and meaning of symbols")
+st.image(img3, caption="", width=1000)
+
+
+# razmak
+st.markdown("<br><br>", unsafe_allow_html=True)
+
+
+# 6) Četvrta slika veća
+st.subheader("Statistical test for residuals and estimation of error")
+st.image(img3, caption="", width=1000)
+
+
+# razmak
+st.markdown("<br><br>", unsafe_allow_html=True)
+
+
+
+
+
